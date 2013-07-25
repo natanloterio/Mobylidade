e98d05aba46cec2f5853bf4652c15b1b2feba473
Natan Lotério <natanloterio@gmail.com>
2013-07-25 20:59:04 -0300

    pesquisa

--- inicio.php
+++ inicio.php
@@ -1,4 +1,12 @@
-<?php require_once('sessao.php'); ?>
+<?php require_once('sessao.php');
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'pesquisa.php';
+$header = "Location: http://$host$uri/$extra";
+//echo "redirecionando para $header";
+header($header);
+
+?>
 
 <!DOCTYPE HTML>
 <html lang="en-US">
