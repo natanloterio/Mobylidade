90bb8906a598384b978a54c4270e0eb41adc1a97
Natan Lotério <natanloterio@gmail.com>
2013-07-26 14:43:50 -0300

    Mostra mensagem "Sem registro"

--- meus_chamados.php
+++ meus_chamados.php
@@ -51,16 +51,27 @@ include_once ("connection.php");
 			  <?
 			  $dadosRota = array();
 			  $consulta=ExecSQL("SELECT * FROM chamados WHERE USUARIOS_ID='".$_COOKIE['USUARIO_ID']."'");
-			  while($dadosConsulta=mysql_fetch_array($consulta)){
-			    $consultaRota=ExecSQL("SELECT * FROM rota WHERE ID='".$dadosConsulta['ROTA_ID']."'");
-			    $dadosRota=mysql_fetch_array($consultaRota);
-			    $cor="";
-			    if($dadosConsulta['STATUS']==1) $cor="Color: #b8b800;"; elseif($dadosConsulta['STATUS']==2) $cor="Color: green;"; elseif($dadosConsulta['STATUS']==3) $cor="Color: red;";
-			  ?>
+			  $total_chamados = mysql_numrows($consulta);
+			  
+			  if($total_chamados > 0){
+			    while($dadosConsulta=mysql_fetch_array($consulta)){
+			      $consultaRota=ExecSQL("SELECT * FROM rota WHERE ID='".$dadosConsulta['ROTA_ID']."'");
+			      $dadosRota=mysql_fetch_array($consultaRota);
+			      $cor="";
+			      if($dadosConsulta['STATUS']==1) $cor="Color: #b8b800;"; elseif($dadosConsulta['STATUS']==2) $cor="Color: green;"; elseif($dadosConsulta['STATUS']==3) $cor="Color: red;";
+			      ?>
 				<li><a href="exibe_chamado.php?chamado_id=<?= $dadosConsulta['CHAMADOS_ID']; ?>"><h3>De <?= $dadosRota['ORIGEM_CIDADE']."-".$dadosRota['ORIGEM_UF']; ?> para <?= $dadosRota['DESTINO_CIDADE']."-".$dadosRota['DESTINO_UF']; ?></h3>
 				<p style="<?= $cor; ?>"><?=  $statusChamados[$dadosConsulta['STATUS']]; ?></p></a>
 				</li>
-			  <? } ?>
+			      <?
+			    }
+			  }else{
+			    ?>
+			      <div class="no_record_found">
+				Nenhum chamado encontrado
+			      </div>
+			    <?
+			  }?>
 			</ul>
 	</div>
 	<!-- Fim conteudo da pagina -->  
